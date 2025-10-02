09546a8f33ee1f20e14c74b485ad73dfa828f9f2
Tomiwa Adelae <thetommedia@gmail.com>
2025-10-02 14:16:40 +0100

    Started working on the assessment page

--- app/(admin)/a/assessment/page.tsx
+++ app/(admin)/a/assessment/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PageHeader } from "../_components/PageHeader";
 import { IconDownload, IconPlus } from "@tabler/icons-react";
+import { AssessmentCards } from "../_components/AssessmentCards";
 
 const page = () => {
   return (
@@ -19,6 +20,7 @@ const page = () => {
           icon: IconDownload,
         }}
       />
+      <AssessmentCards />
     </div>
   );
 };
