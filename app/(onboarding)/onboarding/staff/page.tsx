df38bf61ddcb36abd6954144b39c91e6ed32a959
Tomiwa Adelae <thetommedia@gmail.com>
2025-11-14 20:58:42 +0100

    Worked around the suspense boundary

--- app/(onboarding)/onboarding/staff/page.tsx
+++ app/(onboarding)/onboarding/staff/page.tsx
@@ -1,72 +1,7 @@
-"use client";
-export const dynamic = "force-dynamic";
+// This is a server component
 
-import { notFound, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { School } from "@/store/useAuth";
-import { configService } from "@/lib/configs";
-import { schoolService } from "@/lib/school";
-import { toast } from "sonner";
-import { Loader } from "@/components/Loader";
-import { FullLogo } from "@/app/(auth)/_components/Logo";
-import { OnboardingStaffForm } from "../../_components/OnboardingStaffForm";
+import ClientOnboardingStaff from "../../_components/ClientOnboardingStaff";
 
 export default function page() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
-
-  const [jobRoles, setJobRoles] = useState<any>();
-  const [states, setStates] = useState<any>();
-  const [countries, setCountries] = useState<any>();
-  const [school, setSchool] = useState<School>();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (!id) return;
-
-    const fetchData = async () => {
-      try {
-        const [jobRolesRes, countriesRes, statesRes, schoolRes] =
-          await Promise.all([
-            configService.getCategory("JOB_ROLE"),
-            configService.getCategory("COUNTRY"),
-            configService.getCategory("STATE"),
-            schoolService.getSchool(id),
-          ]);
-
-        setJobRoles(jobRolesRes);
-        setStates(statesRes);
-        setCountries(countriesRes);
-        setSchool(schoolRes);
-      } catch (error: any) {
-        toast.error(error?.response?.data?.message || "Something went wrong");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [id]);
-
-  if (loading) return <Loader />;
-  if (!school) return notFound();
-
-  return (
-    <div className="space-y-4 w-full">
-      <div className="flex items-center justify-center">
-        <FullLogo
-          name={school.name}
-          acronym={school.acronym!}
-          logo={school.logo!}
-        />
-      </div>
-      <OnboardingStaffForm
-        jobRoles={jobRoles.items}
-        countries={countries.items}
-        states={states.items}
-        schoolID={school.schoolID!}
-        acronym={school.acronym!}
-      />
-    </div>
-  );
+  return <ClientOnboardingStaff />;
 }
--- app/(onboarding)/onboarding/student/page.tsx
+++ app/(onboarding)/onboarding/student/page.tsx
@@ -1,77 +1,8 @@
-"use client";
+// This is a server component
 export const dynamic = "force-dynamic";
 
-import { notFound, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { configService } from "@/lib/configs";
-import { schoolService } from "@/lib/school";
-import { toast } from "sonner";
-import { Loader } from "@/components/Loader";
-import { FullLogo } from "@/app/(auth)/_components/Logo";
-import { OnboardingStudentForm } from "../../_components/OnboardingStudentForm";
-import { School } from "@/store/useAuth";
+import ClientOnboardingStudent from "../../_components/ClientOnboardingStudent";
 
 export default function page() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get("id");
-
-  const [classLevels, setClassLevels] = useState<any>([]);
-  const [departments, setDepartments] = useState<any>([]);
-  const [states, setStates] = useState<any>([]);
-  const [countries, setCountries] = useState<any>([]);
-  const [school, setSchool] = useState<School>();
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetch = async () => {
-      try {
-        const [countries, states, classLevels, departments, school] =
-          await Promise.all([
-            configService.getCategory("COUNTRY"),
-            configService.getCategory("STATE"),
-            configService.getCategory("CLASS_LEVEL"),
-            configService.getCategory("SCHOOL_DEPARTMENT"),
-            schoolService.getSchool(id!),
-          ]);
-
-        setCountries(countries);
-        setStates(states);
-        setClassLevels(classLevels);
-        setDepartments(departments);
-
-        setSchool(school);
-      } catch (error: any) {
-        toast.error(error.response.data.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetch();
-  }, [id]);
-
-  if (loading) return <Loader />;
-
-  if (!school) return notFound();
-
-  return (
-    <div className="space-y-4 w-full">
-      <div className="flex items-center justify-center">
-        <FullLogo
-          name={school.name}
-          acronym={school.acronym!}
-          logo={school.logo!}
-        />
-      </div>
-      <OnboardingStudentForm
-        countries={countries.items}
-        states={states.items}
-        schoolID={school?.schoolID!}
-        acronym={school.acronym!}
-        classLevels={classLevels.items}
-        departments={departments.items}
-      />
-    </div>
-  );
+  return <ClientOnboardingStudent />;
 }
